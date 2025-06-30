10557ec595e87365844bddea04fd1c4977339f0d
Kestrel280 <downey.samp@gmail.com>
2025-06-30 13:00:05 -0400

    Improves Player movement (based on Source movement)

---
+++ game_ui.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _process(delta: float) -> void:
+	$SpeedBox/SpeedLabel.text = "%.1f" % (Globals.playerSpeedXy / 0.0254);
--- player.gd
+++ player.gd
@@ -1,20 +1,23 @@
 extends CharacterBody3D
 
+const INCHES_PER_METER : float = 39.3701;
+
+@export var groundFriction = 4;
 @export var mass = 70;
-@export var maxGroundVel = 30;
-@export var groundAccel = 10;
-@export var airFriction = 0.1;
-@export var groundFriction = 5;
+
 @export var jumpImpulse = 4;
-var MOUSE_SENSITIVITY = 0.006; # TODO move to a globals/settings
+@export var airAccel = 150;
+@export var airSpeedCap = 30 / INCHES_PER_METER;
+@export var groundFrictionStopSpeedThreshold = 0.5 / INCHES_PER_METER; # On ground, if speed less than this value, just set to 0
+@export var groundAccel = 5; # Ground acceleration
+@export var groundSpeedCap = 320 / INCHES_PER_METER; # Max walking speed on ground
+var MOUSE_SENSITIVITY = 0.004; # TODO move to a globals/settings
 
 var rot_x = 0	# Cumulative rotation
 var rot_y = 0	# Cumulative rotation
-var wishdir;
+var inputDir : Vector3;
+var outWishVel : Vector3;
 var dt;
-
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
 
 func _ready() -> void:
 	velocity = Vector3.ZERO;
@@ -24,35 +27,63 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	dt = delta;
 
-	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-	wishdir = ($Pivot.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var _inputDir2d = Input.get_vector("move_left", "move_right", "move_forward", "move_backward");
+	inputDir = ($Pivot.transform.basis * Vector3(_inputDir2d.x, 0, _inputDir2d.y)).normalized();
 	_playerMove();
 
-	move_and_slide()
 
 func _playerMove() -> void:
+	outWishVel = Vector3(velocity);
 	if not is_on_floor():
 		_airMove();
 	else:
-		_groundMove();
 		if Input.is_action_pressed("jump"):
 			_jump()
+			_airMove()
+		else:
+			_groundMove()
+	velocity = outWishVel;
+	Globals.playerSpeedXy = Vector2(velocity.x, velocity.z).length();
+	move_and_slide();
 	return;
 
 
 func _airMove() -> void:
-	velocity += get_gravity() * dt;
-	velocity = velocity - (velocity * airFriction * dt);
+	_applyAirAccel();
+	outWishVel += get_gravity() * dt;
+
+
+func _applyAirAccel():
+	var curSpeedInInputDir = inputDir.dot(velocity);
+	var maxSpeedToAdd = max(0, airSpeedCap - curSpeedInInputDir);
+	var speedToAddInInputDir = clampf(airAccel * airSpeedCap * dt, 0.0, maxSpeedToAdd); # TODO multiplication by airSpeedCap should probably be an analog value
+	print("curSpeedInInputDir = %3.1f | maxSpeedToAdd = %3.1f | speedToAddInInputDir = %3.1f" % [curSpeedInInputDir, maxSpeedToAdd, speedToAddInInputDir]);
+	outWishVel += speedToAddInInputDir * inputDir;
 
 
 func _groundMove() -> void:
-	velocity += (wishdir * groundAccel) * dt;
-	velocity = velocity - (velocity * groundFriction * dt);
-	velocity.clampf(-maxGroundVel, maxGroundVel);
+	_applyGroundFriction(); # Apply friction, updating outWishVel
+	_applyGroundAccel();	# Apply ground acceleration, updating outWishVel
+	
 
+func _applyGroundFriction() -> void:
+	var wishSpeed = outWishVel.length();
+	if wishSpeed < groundFrictionStopSpeedThreshold:
+		outWishVel = Vector3.ZERO;
+		return;
+	var speedScale = 1 - (getGroundFriction() * dt);
+	outWishVel = outWishVel * speedScale;
+
+
+func _applyGroundAccel() -> void:
+	var curSpeedInInputDir = inputDir.dot(velocity);
+	var maxSpeedToAddInInputDir = groundSpeedCap - curSpeedInInputDir;
+	var speedToAddInInputDir = clampf(groundAccel * groundSpeedCap * dt, 0.0, maxSpeedToAddInInputDir);  # TODO multiplication by groundSpeedCap should probably be an analog value
+	outWishVel += speedToAddInInputDir * inputDir;
+	
 
 func _jump() -> void:
-	velocity.y += jumpImpulse;
+	outWishVel.y += jumpImpulse;
 
 
 func _input(event):
@@ -69,3 +100,6 @@ func _handleMouseMotionEvent(event):
 	$Pivot.rotate_object_local(Vector3(0, 1, 0), rot_x);
 	$Pivot/Camera.transform.basis = Basis();
 	$Pivot/Camera.rotate_object_local(Vector3(1, 0, 0), rot_y);
+
+func getGroundFriction() -> float:
+	return groundFriction;
